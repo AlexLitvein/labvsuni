c5b0920fdc1a8438b8242c58cec9351a17ca956b
Литвиновский Алексей Борисович <LITVINOVSKIY-AB@sibir.FSK-EES.LOCAL>
2021-02-17 15:31:27 +0700

    end dev msgBox 1

--- public/weather/js/UIElements.js
+++ public/weather/js/UIElements.js
@@ -1,6 +1,19 @@
 function UIElms (oStyles) {
     const self = this;
     // const styles = oStyles;
+    // this.delay = async function (ms) {
+    //     const start = Date.now();
+    //     let now = start;
+    //     while (now - start < ms) {
+    //         now = Date.now();
+    //     }
+    // };
+
+    this.delay = function (ms) { // use - await delay(xxx)
+        return new Promise(resolve => {
+            setTimeout(resolve, ms);
+        });
+    }
 
     this.UICreateMany = function (arrPrms, constr) {
         const arrOut = [];
@@ -11,24 +24,14 @@ function UIElms (oStyles) {
         return arrOut;
     }
 
-    // this.UICreateContainers = function (name, clsCss) {
-    //     const arrOut = [];
-    //     for (let y = 2; y < arguments.length; y += 2) {
-    //         // const h = arguments[y];
-    //         const mdiv = new this.UICont(name + y, clsCss, arguments[y], arguments[y + 1]);
-    //         arrOut.push(mdiv);
-    //     }
-    //     return arrOut;
-    // }
-
-    this.UICreateElement = function (tag, clsCss = '', text = '') {
+    this.UICreateHtmlElm = function (tag, clsCss = '', text = '') {
         const elm = document.createElement(tag);
         if (text) elm.innerText = text;
         if (clsCss) elm.className = clsCss;
         return elm;
     }
 
-    this.addReq = function (elm, eAct, sUrl, oParams, cbFunc) {
+    this.createReq = function (elm, eAct, sUrl, oParams, cbFunc) {
         let outElm = elm;
         if (elm.name) {
             outElm = elm.elm();
@@ -46,7 +49,23 @@ function UIElms (oStyles) {
         });
     }
 
+    this.createFakeReq = function (elm, eAct, sUrl, oParams, cbBefore, cbAfter) {
+        let outElm = elm;
+        if (elm.name) {
+            outElm = elm.elm();
+        }
+
+        outElm.addEventListener(eAct, async function () {
+            cbBefore();
+            await self.delay(2000);
+            cbAfter(oParams);
+        });
+    }
+
     const UIProto = {
+        name: function () { return this.uiCont.nm; }, // стрелочные не канают
+        elm: function () { return this.uiCont.helm; },
+
         addElm: function () {
             for (let y = 0; y < arguments.length; y++) {
                 const elm = arguments[y];
@@ -63,152 +82,121 @@ function UIElms (oStyles) {
             this.elm().className = clsNames + ' ' + this.elm().className;
         },
         toggleAnim: function (kfName) {
-            // this.elm().className = this.elm().className + ' ' + clsName;
-            let an = this.elm().style.animationName;
-            if (an === '') an = kfName;
-            else an = '';
-            this.elm().style.animationName = an;
+            this.elm().style.animationName = this.elm().style.animationName === '' ? kfName : '';
         },
-        tglVis: function () {
+        tglVis: function (ms) {
             const vis = this.elm().style.visibility;
             if (vis === 'visible') {
                 this.elm().style.opacity = 0;
                 // transition: opacity 0.4s ease-out 0.2s; 0.4+0.2=0.6(600)
-                setTimeout(() => { this.elm().style.visibility = 'hidden'; }, 600);
+                setTimeout(() => { this.elm().style.visibility = 'hidden'; }, ms);
             } else {
                 this.elm().style.opacity = 1;
-                setTimeout(() => { this.elm().style.visibility = 'visible'; }, 600);
+                // setTimeout(() => { this.elm().style.visibility = 'visible'; }, ms);
+                this.elm().style.visibility = 'visible';
             }
         },
         hide: function (ms, f) {
             setTimeout(() => {
                 this.elm().style.visibility = 'hidden';
+                this.elm().style.opacity = 0;
                 if (f) f();
             }, ms);
         }//,
-
-        // addAct: function (eAct, sUrl, oParams, cbFunc) {
-        //     this.elm().addEventListener(eAct, async function () {
-        //         const response = await fetch(sUrl, {
-        //             method: 'POST',
-        //             headers: { 'Content-Type': 'application/json;charset=utf-8' },
-        //             body: JSON.stringify(oParams)
-        //         });
-
-        //         const result = await response.json();
-        //         cbFunc(result);
-        //         // console.log('xmlreq: ' + result);
-        //     });
-        // }
     }
 
     this.UICont = function (name, clsName, h = '', w = '') {
-        const nm = name;
-        const htmlelm = document.createElement('div');
-        this.name = () => nm;
-        this.elm = () => htmlelm;
+        this.nm = name;
+        this.helm = document.createElement('div');
+        this.name = function () { return this.nm; }
+        this.elm = function () { return this.helm; }
         if (h) {
-            htmlelm.style.height = h;
+            this.helm.style.height = h;
             // htmlelm.style.minHeight = h;
         }
         if (w) {
             // htmlelm.style.width = w;
-            htmlelm.style.minWidth = w;
+            this.helm.style.minWidth = w;
         }
         this.addClass(clsName); // at the end
     }
     this.UICont.prototype = UIProto;
 
     this.UIForm = function (name, clsName, h = '', w = '') {
-        const nm = name;
-        const htmlelm = document.createElement('form');
-        this.name = () => nm;
-        this.elm = () => htmlelm;
+        this.nm = name;
+        this.helm = document.createElement('form');
+        this.name = function () { return this.nm; }
+        this.elm = function () { return this.helm; }
         if (h) {
-            htmlelm.style.height = h;
+            this.helm.style.height = h;
             // htmlelm.style.minHeight = h;
         }
         if (w) {
             // htmlelm.style.width = w;
-            htmlelm.style.minWidth = w;
+            this.helm.style.minWidth = w;
         }
         this.addClass(clsName); // at the end
     }
     this.UIForm.prototype = UIProto;
 
-    this.UILegendItem = function (name, text, clsName, offsX = 0, offsY = 0) {
-        const uiCont = new self.UICont(name, 'flxCentH');
-        this.name = () => uiCont.name();
-        this.elm = () => uiCont.elm();
-        this.text = document.createElement('span');
+    this.UILegendItem = function (name, text, imgClsName, dir = 'right', offsX = 0, offsY = 0) {
+        this.uiCont = new self.UICont(name, 'flxCnt');
+        this.setImgClass = (clsName) => { this.img.className = clsName; };
+        this.setTxtClass = (clsName) => { this.txt.className = clsName; };
 
+        if (dir === 'left') this.uiCont.elm().style.flexDirection = 'row-reverse';
+        if (dir === 'top') this.uiCont.elm().style.flexDirection = 'column-reverse';
+        if (dir === 'bottom') this.uiCont.elm().style.flexDirection = 'column';
+
+        this.txt = document.createElement('span');
+        this.txt.innerText = text;
         this.img = document.createElement('image');
-        this.img.className = clsName;
+        // this.img.className = imgClsName;
+        this.setImgClass(imgClsName);
         this.img.style.backgroundPositionX = offsX;
         this.img.style.backgroundPositionY = offsY;
-        uiCont.addElm(this.img);
-
-        this.text.innerText = text;
-        uiCont.addElm(this.text);
+        this.uiCont.addElm(this.img, this.txt);
     }
     this.UILegendItem.prototype = UIProto;
 
     this.UIImage = function (name, clsName, url, h = '', w = '') {
-        const uiCont = new self.UICont(name, clsName, h, w);
-        this.name = () => uiCont.name();
-        this.elm = () => uiCont.elm();
-
-        if (url) {
-            const img = document.createElement('img');
-            img.src = url;
-            uiCont.addElm(img);
-        }
+        this.uiCont = new self.UICont(name, clsName, h, w);
+        // if (url) {
+        const img = document.createElement('img');
+        img.src = url;
+        this.uiCont.addElm(img);
+        // }
     }
     this.UIImage.prototype = UIProto;
 
     this.UITextButton = function (name, text, clsName, h = '', w = '') {
-        const uiCont = new self.UICont(name, clsName, h, w);
-        this.name = () => uiCont.name();
-        this.elm = () => uiCont.elm();
-
-        // this.doClick = function () { console.log('doClick'); }
-        // this.elm().addEventListener('click', this.doClick);
-        // this.elm().addEventListener('mouseenter', (evt) => this.addClass('btnOver'));
-        // this.elm().addEventListener('mouseleave', (evt) => this.addClass('btnNorm'));
-        // this.elm().addEventListener('mousedown', (evt) => this.addClass('btnDown'));
-        // this.elm().addEventListener('click', (evt) => this.addClass('btnUp'));
-
+        this.uiCont = new self.UICont(name, clsName, h, w);
         const txt = document.createElement('span');
         txt.innerText = text;
-        uiCont.addElm(txt);
+        this.uiCont.addElm(txt);
     }
     this.UITextButton.prototype = UIProto;
 
     this.UIImgButton = function (name, clsName, offsX = 0, offsY = 0) {
-        const uiCont = new self.UICont(name, clsName);
-        this.name = () => uiCont.name();
-        this.elm = () => uiCont.elm();
+        this.uiCont = new self.UICont(name, clsName);
+        // this.name = () => uiCont.name();
+        // this.elm = () => uiCont.elm();
 
         const img = document.createElement('image');
         img.className = 'sysImg16';
         img.style.backgroundPositionX = offsX;
         img.style.backgroundPositionY = offsY;
-        uiCont.addElm(img);
+        this.uiCont.addElm(img);
     }
     this.UIImgButton.prototype = UIProto;
 
-    // setAttribute('type', 'text') password text email / pattern
-    // function (name, type, pattern, label, title, offsX = 0, offsY = 0)
     this.UITextEditValid = function (name, label, title, offsX = 0, offsY = 0) {
-        const uiCont = new self.UICont(name, 'flxCentH cntEnd');
-        this.name = () => uiCont.name();
-        this.elm = () => uiCont.elm();
-
-        const lab = self.UICreateElement('span', '', label);
-        this.inp = document.createElement('input');
-        this.inp.className = 'myInput';
-        this.inp.setAttribute('type', 'text');
-        this.inp.pattern = '^[а-яА-Яa-zA-Z0-9]{3,10}$';
+        this.uiCont = new self.UICont(name, 'flxCentH cntEnd');
+        const lab = self.UICreateHtmlElm('span', '', label);
+        const inp = document.createElement('input');
+        inp.className = 'myInput';
+        inp.setAttribute('type', 'text');
+        inp.pattern = '^[а-яА-Яa-zA-Z0-9]{3,10}$';
 
         const img = document.createElement('image');
         img.className = 'sysImg16';
@@ -216,17 +204,80 @@ function UIElms (oStyles) {
         img.style.backgroundPositionY = offsY;
         if (title) img.title = title;
 
-        uiCont.addElm(lab, this.inp, img);
+        this.uiCont.addElm(lab, inp, img);
     }
     this.UITextEditValid.prototype = UIProto;
 
-    this.UIWaitMsg = function (name, clsName, text, offsX = 0, offsY = 0) {
-        const uiCont = new self.UILegendItem(name, text, clsName + ' ' + 'anim', offsX, offsY);
-        this.name = () => uiCont.name();
-        this.elm = () => uiCont.elm();
-
-        uiCont.setClass('flxCnt posAbs tranOpac');
-        uiCont.elm().style.visibility = 'hidden'; // для функции скрытия
+    this.UIMsgBox = function (name, text, url = '', txtButt = '', cb) {
+        this.uiCont = new self.UICont(name, 'flxColCnt posAbs tranOpac');
+        this.hide(0);
+        this.txt = this.img = this.mButt = null;
+        const szImg = '32px';
+        if (url) {
+            this.img = new self.UIImage('img1', 'msgBoxImg anim', url, szImg, szImg);
+            this.uiCont.addElm(this.img);
+        }
+        if (text) {
+            this.txt = new self.UICreateUIElm('text', 'p', text, 'msgBoxHeader');
+            this.uiCont.addElm(this.txt);
+        }
+        if (txtButt) {
+            this.mButt = new self.UITextButton('buttOk', 'Ok', 'btnInit', '10px', '52px');
+            this.uiCont.addElm(this.mButt);
+            if (cb) { this.mButt.elm().addEventListener('click', cb); }
+        }
+        this.toggleAnim = function (kfName) {
+            if (this.img) this.img.toggleAnim(kfName);
+        }
     }
-    this.UIWaitMsg.prototype = UIProto;
+    this.UIMsgBox.prototype = UIProto;
+
+    this.UIPopupWnd = function (name) {
+        this.uiCont = new self.UICont(name, 'popUpBgd flxCnt'); // фоновый контейнер
+        this.uiCont.elm().style.visibility = 'hidden'; // для функции скрытия
+        // this.uiCont.hide(0);
+        let dlgCont = new self.UICont('dlgCont', 'dlgCont bkg');
+        this.uiCont.addElm(dlgCont);
+        let currShow = null;
+
+        this.close = function () {
+            currShow = null;
+            this.uiCont.elm().style.visibility = 'hidden';
+            this.uiCont.elm().removeChild(dlgCont.elm());
+            delete this.dlgCont;
+
+            dlgCont = new self.UICont('dlgCont', 'dlgCont bkg');
+            this.uiCont.addElm(dlgCont);
+        }
+        // this.show = function (uielm, ms) {
+        //     this.uiCont.elm().style.visibility = 'visible';
+        //     if (currShow !== null) currShow.tglVis(ms);
+        //     uielm.tglVis(ms);
+        //     currShow = uielm;
+        // }
+        this.show = function (elmName, ms) {
+            this.uiCont.elm().style.visibility = 'visible';
+            if (currShow !== null) currShow.tglVis(ms);
+            const e1 = dlgCont[elmName];
+            if (e1) {
+                e1.tglVis(ms);
+                currShow = e1;
+            }
+            return e1;
+        }
+        this.getItem = function (itemName) {
+            return dlgCont[itemName];
+        }
+        this.addElm = function () {
+            dlgCont.addElm(...arguments);
+        }
+    }
+    this.UIPopupWnd.prototype = UIProto;
+
+    this.UICreateUIElm = function (name, tag, text = '', clsCss = '') {
+        this.uiCont = new self.UICont(name, 'flxCnt');
+        this.item = self.UICreateHtmlElm(tag, clsCss, text);
+        this.uiCont.addElm(this.item);
+    }
+    this.UICreateUIElm.prototype = UIProto;
 }
--- public/weather/js/chat.js
+++ public/weather/js/chat.js
@@ -53,8 +53,8 @@
 
         const prmLegnd = [];
         prmLegnd.push(['lg1', '8888', 'lgndImg16']);
-        prmLegnd.push(['lg2', '9999', 'lgndImg16', '16px', '0px']);
-        prmLegnd.push(['lg3', '9999', 'lgndImg16', '32px', '0px']);
+        prmLegnd.push(['lg2', '9999', 'lgndImg16', 'right', '16px', '0px']);
+        prmLegnd.push(['lg3', '9999', 'lgndImg16', 'right', '32px', '0px']);
         // prmLegnd.push(['lg4', '9999', 'flxCntGrw', '16px', '48px', '0px']);
         const lgnds = uie.UICreateMany(prmLegnd, uie.UILegendItem);
         rPanBot.addElm(...lgnds);
@@ -64,7 +64,7 @@
     }
 
     function createMenu (name) {
-        const cont = new uie.UICont(name, 'flxGrw', '32px');
+        const cont = new uie.UICont(name, 'flx', '32px');
 
         const ibut1 = new uie.UITextButton('ibut1', 'Add', 'btnInit', '', '52px');
         const ibut2 = new uie.UITextButton('ibut2', 'Reg', 'btnInit', '', '52px');
@@ -72,8 +72,8 @@
 
         ibut1.elm().addEventListener('click', async function () {
             const dia = createDialog();
-            controls.body.addElm(dia);
-            addCtrl(dia);
+            // controls.body.addElm(dia);
+            // addCtrl(dia);
 
             // const response = await fetch('/weather/chat', {
             //     method: 'POST',
@@ -85,7 +85,7 @@
             // console.log('xmlreq: ' + result);
         });
 
-        uie.addReq(ibut2, 'click', '/weather/chat', { f: 'UserRegzd08hvlKhwfu', params: ['userLogin', 'password'] }, res => { console.log('xmlreq: ' + res); })
+        uie.createReq(ibut2, 'click', '/weather/chat', { f: 'UserRegzd08hvlKhwfu', params: ['userLogin', 'password'] }, res => { console.log('xmlreq: ' + res); })
 
         return cont;
     }
@@ -133,49 +133,69 @@
     }
 
     function createRegForm () {
-        const cont = new uie.UICont('contRegForm', ' tranOpac'); // posAbs
-        cont.elm().style.visibility = 'visible'; // для функции скрытия
-
+        const cont = new uie.UICont('regForm', ' tranOpac'); // posAbs
+        cont.elm().style.visibility = 'hidden'; // для функции скрытия
+        const regHdr = new uie.UICreateUIElm('regHdr', 'h1', 'Регистрация');
         const login = new uie.UITextEditValid('login', 'Логин', '3-5 символов русского, латинского алфавита, цифры');
 
         // uie.addReq(login.inp, 'click', '/weather/chat', { f: 'UserRegzd08hvlKhwfu', params: ['userLogin', 'password'] }, res => { login.inp.value = res; })
 
         const passw1 = new uie.UITextEditValid('passw1', 'Пароль', '3-5 символов латинского алфавита, цифры');
-
         const passw2 = new uie.UITextEditValid('passw2', 'Пароль ещё раз', '3-5 символов латинского алфавита, цифры');
-        cont.addElm(login, passw1, passw2);
+        cont.addElm(regHdr, login, passw1, passw2); // header,
 
+        const buttPan = new uie.UICont('buttPan', 'flxCnt paddT');
+        const butOk = new uie.UITextButton('butOk', 'Ок', 'btnInit', '', '52px');
+        const butCancel = new uie.UITextButton('butCancel', 'Отмена', 'btnInit', '', '52px');
+        buttPan.addElm(butOk, butCancel);
+        cont.addElm(buttPan);
         return cont;
     }
 
     function createLoginForm () {
         // const cont = new uie.UICont('contLogForm', 'posAbs tranOpac');
         const cont = new uie.UIForm('logForm', 'posAbs tranOpac');
-        cont.elm().style.visibility = 'visible'; // для функции скрытия
-
+        cont.elm().style.visibility = 'hidden'; // для функции скрытия
+        const logHdr = new uie.UICreateUIElm('logHdr', 'h1', 'Вход');
         const login = new uie.UITextEditValid('login', 'Логин', '3-5 символов русского, латинского алфавита, цифры');
 
         // uie.addReq(login.inp, 'click', '/weather/chat', { f: 'UserRegzd08hvlKhwfu', params: ['userLogin', 'password'] }, res => { login.inp.value = res; })
 
         const passw = new uie.UITextEditValid('passw', 'Пароль', '3-5 символов латинского алфавита, цифры');
-        cont.addElm(login, passw);
-        return cont;
-    }
+        cont.addElm(logHdr, login, passw);
 
-    function creaWaitForm (text) {
-        // const img = new uie.UIImage('waitImg', 'posAbs tranOpac anim', '/weather/img/avat_01.png');
-        // img.elm().style.visibility = 'hidden'; // для функции скрытия
+        const buttPan = new uie.UICont('buttPan', 'flxCnt paddT');
+        const butOk = new uie.UITextButton('butOk', 'Ок', 'btnInit', '', '52px');
+        const butCancel = new uie.UITextButton('butCancel', 'Отмена', 'btnInit', '', '52px');
+        buttPan.addElm(butOk, butCancel);
+        cont.addElm(buttPan);
 
-        // const msg = uie.UICreateElement('span', '', text);
-        // img.addElm(msg);
-        const cont = new uie.UIWaitMsg('wait1', 'lgndImg16', text);
+        let f = null;
+        function before (params) {
+            f = controls.mainDlg.show('waitLog', 600);
+            f.toggleAnim('example');
+        }
+        function after (res) {
+            if (res.stat === 'Ok') {
+                f = controls.mainDlg.show('logOk', 600);
+                f.hide(2000, () => { controls.mainDlg.close(); });
+            } else {
+                f = controls.mainDlg.show('logFail', 600);
+            }
+        }
+
+        uie.createFakeReq(butOk, 'click', '', { stat: 'Ook' }, before, after);
+
+        butCancel.elm().addEventListener('click', function () {
+            controls.mainDlg.close();
+        });
 
         return cont;
     }
 
     function createCaptionPan (caption) {
         const cont = new uie.UICont('contCapt', 'captPan');
-        const ico = new uie.UILegendItem('ico', caption, 'lgndImg16', '16px');
+        const ico = new uie.UILegendItem('ico', caption, 'lgndImg16', 'right', '16px');
         cont.addElm(ico);
         const rButt1 = new uie.UIImgButton('rButt1', 'btnInit', '-17px');
         const rButt2 = new uie.UIImgButton('rButt2', 'btnInit', '-17px');
@@ -188,45 +208,78 @@
         return cont;
     }
 
+    // function createDialog (params) {
+    //     const cont = new uie.UICont('logRegDlg', 'popUpBgd flxCnt'); // фоновый контейнер
+    //     const popUpCont = new uie.UICont('popUpCont', 'flxColCntH bkg'); // dlg
+    //     cont.addElm(popUpCont);
+
+    //     const capt = createCaptionPan('Hello! Caption!');
+
+    //     // NOTE: тело диалога подстраивается под размер содержимого, но тк содержимое с
+    //     // абсолютной позиц, то тело схлопываетя в 0. По этому делаем контейнер
+    //     // (с самым большим содержимым) с обычныой позицией и скрываем его если надо,
+    //     // остальные с абсолютной
+    //     const dlgBodyCont = new uie.UICont('dlgBodyCont', 'flxCntGrw posRel');  // dlgBody
+    //     const logForm = createLoginForm();
+    //     const regForm = createRegForm();
+    //     // const waitImg = creaWaitForm('Waiting...');
+    //     const waitImg = new uie.UIWaitMsg('wait1', 'msgBoxImg', 'Waiting...', '', 'bottom');
+    //     regForm.tglVis();
+    //     // waitImg.tglVis();
+
+    //     dlgBodyCont.addElm(logForm, regForm, waitImg);
+
+    //     const buttPan = new uie.UICont('buttPan', 'flxCnt');
+
+    //     const butOk = new uie.UITextButton('butOk', 'Ок', 'btnInit', '', '52px');
+    //     const butCancel = new uie.UITextButton('butCancel', 'Отмена', 'btnInit', '', '52px');
+    //     buttPan.addElm(butOk, butCancel);
+
+    //     butOk.elm().addEventListener('click', function () {
+    //         logForm.tglVis();
+    //         // regForm.tglVis();
+    //         waitImg.tglVis();
+    //         waitImg.toggleAnim('example');
+    //         waitImg.hide(2000, () => {
+    //             logForm.tglVis();
+    //             waitImg.toggleAnim();
+    //         }); // remCtrl('logRegDlg');
+    //     });
+
+    //     butCancel.elm().addEventListener('click', function () {
+    //         remCtrl('logRegDlg');
+    //     });
+
+    //     popUpCont.addElm(capt, dlgBodyCont, buttPan);
+    //     return cont;
+    // }
+
     function createDialog (params) {
-        const cont = new uie.UICont('logRegDlg', 'popUpBgd flxCnt'); // фоновый контейнер
-        const popUpCont = new uie.UICont('popUpCont', 'flxColCntH bkg'); // dlg
-        cont.addElm(popUpCont);
-
-        const capt = createCaptionPan('Hello! Caption!');
-
-        // NOTE: тело диалога подстраивается под размер содержимого, но тк содержимое с
-        // абсолютной позиц, то тело схлопываетя в 0. По этому делаем контейнер
-        // (с самым большим содержимым) с обычныой позицией и скрываем его если надо,
-        // остальные с абсолютной
-        const dlgBodyCont = new uie.UICont('dlgBodyCont', 'flxCntGrw posRel');  // dlgBody
         const logForm = createLoginForm();
         const regForm = createRegForm();
-        const waitImg = creaWaitForm('Waiting...');
-        regForm.tglVis();
-        // waitImg.tglVis();
+        // const waitForm = new uie.UIMsgBox('waitLog', 'msgBoxImg', 'Waiting...', '', 'bottom');
+        const waitLog = new uie.UIMsgBox('waitLog', 'Ожидаем...', 'xxx');
+        const logOk = new uie.UIMsgBox('logOk', 'Вход выполнен.', 'xxx');
+        const logFail = new uie.UIMsgBox('logFail', 'Ошибка при входе. Попробуйте позже.', 'xxx', 'Ok', () => { controls.mainDlg.close(); });
 
-        dlgBodyCont.addElm(logForm, regForm, waitImg);
+        controls.mainDlg.addElm(logForm, regForm, waitLog, logOk, logFail);
+        controls.mainDlg.show('logForm', 0); // regForm
 
-        const buttPan = new uie.UICont('buttPan', 'flxCnt');
+        // const buttPan = new uie.UICont('buttPan', 'flxCnt');
+        // const butOk = new uie.UITextButton('butOk', 'Ок', 'btnInit', '', '52px');
+        // const butCancel = new uie.UITextButton('butCancel', 'Отмена', 'btnInit', '', '52px');
+        // buttPan.addElm(butOk, butCancel);
 
-        const butOk = new uie.UITextButton('butOk', 'Ок', 'btnInit', '', '52px');
-        const butCancel = new uie.UITextButton('butCancel', 'Отмена', 'btnInit', '', '52px');
-        buttPan.addElm(butOk, butCancel);
-
-        butOk.elm().addEventListener('click', function () {
-            logForm.tglVis();
-            // regForm.tglVis();
-            waitImg.tglVis();
-            waitImg.uiCont.img.toggleAnim('example');
-        });
-
-        butCancel.elm().addEventListener('click', function () {
-            remCtrl('logRegDlg');
-        });
-
-        popUpCont.addElm(capt, dlgBodyCont, buttPan);
-        return cont;
+        // butOk.elm().addEventListener('click', function () {
+        //     logForm.tglVis();
+        //     // regForm.tglVis();
+        //     waitImg.tglVis();
+        //     waitImg.toggleAnim('example');
+        //     waitImg.hide(2000, () => {
+        //         logForm.tglVis();
+        //         waitImg.toggleAnim();
+        //     }); // remCtrl('logRegDlg');
+        // });
     }
 
     function ititUI () {
@@ -234,26 +287,23 @@
         topParent.appendChild(bodyElm.elm());
         addCtrl(bodyElm);
 
+        const mainDlg = new uie.UIPopupWnd('mainDlg');
+        bodyElm.addElm(mainDlg);
+        addCtrl(mainDlg);
+
         const menu = createMenu('menu');
         const contUser = new uie.UICont('contUser', 'flxGrw', '100%');
-        bodyElm.addElm(menu, contUser);
+        bodyElm.addElm(menu, contUser);//
 
         const lPan = new uie.UICont('lPan', '', '', '250px');
         const rPan = new uie.UICont('rPan', 'flxGrwCol');
         contUser.addElm(lPan, rPan);
 
         const uicards = uie.UICreateMany([[], [], [], []], createUser);
-        // const uicards = uie.UICreateMany([[]], createUser);
+        // // const uicards = uie.UICreateMany([[]], createUser);
         lPan.addElm(...uicards);
 
         const msgInput = createMsgPan('32px');
         rPan.addElm(msgInput);
-
-        // const dia = createDialog();
-        // bodyElm.addElm(dia);
-
-        // console.log(rPan);
-        // const ibut1 = new uie.UIImgButton('ibut1', myStyles.brd1, 'url("avat_03.png")', '32px', '32px', '0px', '0px');
-        // rPan.addElm(ibut1);
     }
 })();
